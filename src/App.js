cdf63e725742ab46211ee3157555ca9b6ebb9f42
unknown <viola4lfe@gmail.com>
2020-01-20 10:35:35 -0500

    brought useContext to Products component, removed props and passed down data using useContext

--- src/App.js
+++ src/App.js
@@ -20,15 +20,11 @@ function App() {
 
   return (
     <div className="App">
-      <ProductContext.Provider value={{products, addItem}}>
+      <ProductContext.Provider value={{ products, addItem }}>
         <Navigation cart={cart} />
 
         {/* Routes */}
-        <Route
-          exact
-          path="/"
-          component={Products}
-        />
+        <Route exact path="/" component={Products} />
 
         <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
       </ProductContext.Provider>
